4ca99fba905d26cc215b4b68bbb2ded305e4eb2b
Arthur-Delacroix <373974021@qq.com>
2020-10-24 10:04:40 +0800

    Resetup project
    重新创建了项目，并且删除了不相关的Unity Package
    同时创建了第一篇文章相应的代码

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 > [教程License](https://catlikecoding.com/unity/tutorials/license/)
 
 已经征求了作者的同意
+![]()
+
+![在独立游戏仓库无后缀](https://raw.github.com/Arthur-Delacroix/IndieGame/master/Documentation/Diagram/Class.svg)
---
+++ UnitySourceCode/Assets/_Scripts/HexMetrics.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public static class HexMetrics
+{
+    //正六边形的边长 同时也是正六边形外接圆的半径
+    public const float outerRadius = 10f;
+
+    //正六边形的内切圆半径，长度为外接圆的 二分之根号三倍
+    public const float innerRadius = outerRadius * 0.866025404f;
+
+    //正六边形的六个顶点位置，从最上面一个顶点开始，顺时针依次定义6个顶点
+    public static Vector3[] corners =
+    {
+        new Vector3(0f, 0f, outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius)
+    };
+}
+

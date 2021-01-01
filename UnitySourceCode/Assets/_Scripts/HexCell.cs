18aca13c0edcfd279b0b54cd0fedbfaea4b91f9e
Arthur-Delacroix <373974021@qq.com>
2021-01-01 16:49:09 +0800

    2-1 相邻的地图单元
    完成了“2-1 相邻的地图单元”部分
    文章未校对

--- UnitySourceCode/Assets/_Scripts/HexCell.cs
+++ UnitySourceCode/Assets/_Scripts/HexCell.cs
@@ -9,6 +9,35 @@ public class HexCell : MonoBehaviour
     //存储cell自身的颜色
     public Color color;
 
+    //用来存储每个cell的neighbors
+    [SerializeField] private HexCell[] neighbors;
+
+    /// <summary>
+    /// 用来获取neighbors中相应方位cell的实例
+    /// 这里注意，虽然HexDirection取值为0-5，neighbors长度为6，不会越界
+    /// 但是不是所有cell都有6个相邻的cell_neighbor，所以可能取出为空，之后会添加判断
+    /// </summary>
+    /// <param name="direction">相邻cell的方位 枚举</param>
+    /// <returns>相应方位cell 的实例</returns>
+    public HexCell GetNeighbor(HexDirection direction)
+    {
+        return neighbors[(int)direction];
+    }
+
+    /// <summary>
+    /// 将相邻的cell实例赋值到neighbors中对应的位置
+    /// 这里注意neighbors的索引下标，应与HexDirection方位的int值对应
+    /// </summary>
+    /// <param name="direction">相邻cell的方位</param>
+    /// <param name="cell">相邻cell的实例</param>
+    public void SetNeighbor(HexDirection direction, HexCell cell)
+    {
+        neighbors[(int)direction] = cell;
+
+        //在赋值自身的neighbors实例的同时，也将自身实例赋值到相邻cell的neighbors数组中
+        cell.neighbors[(int)direction.Opposite()] = this;
+    }
+
     //public Vector3 v1;
     //public Vector3 v2 { get; private set; }
     //void Start()
---
+++ UnitySourceCode/Assets/_Scripts/HexDirection.cs
@@ -0,0 +1,39 @@
+﻿//表示相邻cell方位的枚举
+//从左上角顺时针依次开始
+//参考图：http://magi-melchiorl.gitee.io/pages/Pics/Hexmap/2-1-2.png
+public enum HexDirection
+{
+    NE,
+    E,
+    SE,
+    SW,
+    W,
+    NW
+}
+
+public static class HexDirectionExtensions
+{
+
+    /// <summary>
+    /// 参考图 http://magi-melchiorl.gitee.io/pages/Pics/Hexmap/2-1-4.png
+    /// 根据 相邻cell 位于 自身cell 的位置，获得 自身cell 位于 相邻cell 的位置
+    /// 也就是得到相反位置
+    /// </summary>
+    /// <param name="direction"></param>
+    /// <returns></returns>
+    public static HexDirection Opposite(this HexDirection direction)
+    {
+        //return (int)direction < 3 ? (direction + 3) : (direction - 3);
+
+        //已知了 相邻cell 的位置，自身cell位置与相邻cell位置相反
+        //即 W(1) - E(4)这样的对应关系，之间正好相差3
+        if ((int)direction < 3)
+        {
+            return (direction + 3);
+        }
+        else
+        {
+            return (direction - 3);
+        }
+    }
+}

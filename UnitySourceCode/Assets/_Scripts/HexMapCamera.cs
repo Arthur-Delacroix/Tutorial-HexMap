50dff1ce86ed256373979fcdb2b32681ec5f41db
Arthur-Delacroix <373974021@qq.com>
2021-06-10 17:23:30 +0800

    完成了5-7章的内容
    完成了5-7章的内容，文字部分未校对

---
+++ UnitySourceCode/Assets/_Scripts/HexMapCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class HexMapCamera : MonoBehaviour
+{
+    //控制camera视角旋转
+    [SerializeField] private Transform swivel;
+
+    //控制camera视角远近
+    [SerializeField] private Transform stick;
+
+    void Awake()
+    {
+        //获取对应的实例
+        //swivel = transform.GetChild(0);
+        //stick = swivel.GetChild(0);
+    }
+}
